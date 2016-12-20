76f58122c3f44570ce9109fe2f6dcb6a9f12886b
Julien Besle <julienbesle@gmail.com>
2016-12-20 11:50:19 +0000

    added empty combine/transform overlay function stopInDebugger.m

---
+++ mrLoadRet/Plugin/GLM_v2/combineTransformOverlayFunctions/stopInDebugger.m
@@ -0,0 +1,19 @@
+% stopInDebugger.m: 
+%                     Outputs are -
+%                                 - 
+%                     Additional input argument (optional): 
+%
+%        $Id: stopInDebugger.m 2733 2013-05-13 11:47:54Z julien $
+%           
+
+function varargout=stopInDebugger(varargin)
+
+keyboard
+
+%The input overlays are the first cells of the varargin cell array
+%Do what you want with them and put your result(s) in the varargout array
+%Each output must have the same size as the input overlays
+
+%varargout{1}= yourFirstOverlay;
+%varargout{2}= yourSecondOverlay;
+
